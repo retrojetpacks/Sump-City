dd9e6326f35c303a3b5c16019ba960aae596411f
Jack Humphries <retrojetpacks@gmail.com>
2021-01-05 14:33:51 +0000

    Progress on site using lodash

--- README.txt
+++ README.txt
@@ -1,2 +1,18 @@
 npm install
 Install all modules in package.json. You will commonly clone a site without all the node modules, then need to get the node modules
+
+
+
+Express - route params, dynamic URLs
+app.get("/posts/:anything-:anythingelse.:where", function(req,res){
+  console.log(req.params.anything);
+  console.log(req.params.anythingelse);
+  console.log(req.params.where);
+});
+
+can access parameters from search address, saved to req.params object
+: specifies start of parameter
+- and . are interpreted literally
+
+
+Lodash
--- app.js
+++ app.js
@@ -1,53 +1,93 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash"); //For word processing
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public/'));
 
-const homeContent = ["blah blah"];
 const aboutContent = "blah blah blah";
 const projectsContent = "blah whale blah";
 const contactContent = "blah blah blah blah";
 
+const blogPosts = [];
+
+function parse(str) {
+  return lodash.replace(lodash.toLower(str), '-', '');
+}
 
 
-app.get("/", function(req,res){
-  res.render("home", {content: homeContent});
+app.get("/", function(req, res) {
+  res.render("home", {
+    content: blogPosts
+  });
 });
 
-app.get("/about", function(req,res){
-  res.render("about", {content: aboutContent});
+app.get("/about", function(req, res) {
+  res.render("about", {
+    content: aboutContent
+  });
 });
 
-app.get("/projects", function(req,res){
-  res.render("projects", {content: projectsContent});
+app.get("/projects", function(req, res) {
+  res.render("projects", {
+    content: projectsContent
+  });
 });
 
-app.get("/contacts", function(req,res){
-  res.render("contacts", {content: contactContent});
+app.get("/contacts", function(req, res) {
+  res.render("contacts", {
+    content: contactContent
+  });
 });
 
-app.get("/compose", function(req,res){
-  res.render("compose", {content: contactContent});
+app.get("/compose", function(req, res) {
+  res.render("compose", {
+    content: contactContent
+  });
+});
+
+//Example of express route params
+app.get("/posts/:anything-:anythingelse.:where", function(req, res) {
+  console.log(req.params.anything);
+  console.log(req.params.anythingelse);
+  console.log(req.params.where);
+});
+
+app.get("/posts/:postName", function(req, res) {
+  let reqTitle = parse(req.params.postName);
+
+  console.log(reqTitle);
+
+  // Challenge: word proc into lowercase pure string
+  blogPosts.forEach(function(post) {
+    if (parse(post.title) === reqTitle) {
+      console.log("Match found: " + req.params.postName);
+    } else {
+      console.log("Not a match");
+    }
+  });
+  res.redirect("/");
+
 });
 
 
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.newPostTitle,
+    content: req.body.newPostBody
+  };
+  blogPosts.push(post);
 
-app.post("/compose", function(req,res){
-  let postTitle = req.body.newPostTitle;
-  let post = req.body.newPost;
-
-  homeContent.push(postTitle);
-  homeContent.push(post);
-
-  res.redirect("/compose");
+  res.redirect("/");
 });
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("DR JH running on port 3000");
 });
 
